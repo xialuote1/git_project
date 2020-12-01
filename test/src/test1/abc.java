e1949319670c0fb0d56e8c6cc8c29cd2ba4e87e5
xialuote1 <74592299+xialuote1@users.noreply.github.com>
2020-12-01 17:46:01 +0800

    Update abc.java

--- test/src/test1/abc.java
+++ test/src/test1/abc.java
@@ -1,7 +1,13 @@
 package test1;
 
 public class abc {
+[branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
 
-	private int id;
+
 	
 }
